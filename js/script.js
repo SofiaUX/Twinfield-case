adff614f84e5c780d278f8e2c9a7462ebfc45dcd
SofiaUX <sofoklis2008@gmail.com>
2019-03-25 12:43:00 +0100

    Onclick exit icon

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 console.log("Twinfield case by Sofia Kasimi");
 
-function toggleNavbar() {
+function toggleNavbar(icon) {
   var x = document.getElementById("topnav");
   if (x.className === "topnav") {
     x.className += " responsive";
+    icon.innerHTML = '&#9747;';
   } else {
     x.className = "topnav";
+    icon.innerHTML = '&#9776;';
   }
 }
